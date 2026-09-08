a6e504eb2c402249379bef7a2b31c13f4fdf122e
KurzonDax <5052775+KurzonDax@users.noreply.github.com>
2026-09-08 08:29:57 -0500

    Align cleanup test overrides and reset isolated schema capabilities

--- tests/Integration/ReleaseCleanupSafetyMariaDbTest.php
+++ tests/Integration/ReleaseCleanupSafetyMariaDbTest.php
@@ -238,7 +238,7 @@ class ReleaseCleanupSafetyMariaDbTest extends ReleaseRemoverBatchingTest
                 parent::__construct();
             }
 
-            public function deleteCollectionsAndDescendants(array $collectionIds, string $label = 'CBP cleanup', bool $echoCLI = false): int
+            public function deleteCollectionsAndDescendants(array $collectionIds, string $label = 'CBP cleanup', bool $echoCLI = false, ?int $expectedReleaseId = null): int
             {
                 $this->called = true;
                 ($this->attempt)();
@@ -246,7 +246,7 @@ class ReleaseCleanupSafetyMariaDbTest extends ReleaseRemoverBatchingTest
                     throw new \RuntimeException('Injected CBP cleanup failure');
                 }
 
-                return parent::deleteCollectionsAndDescendants($collectionIds, $label, $echoCLI);
+                return parent::deleteCollectionsAndDescendants($collectionIds, $label, $echoCLI, $expectedReleaseId);
             }
         };
         $release = Release::query()->findOrFail(1);
