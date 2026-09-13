4d3e7c0b6f1f103496a1c76679afe14402ea72eb
KurzonDax <randy@thewilliscrew.org>
2026-09-13 17:59:46 -0500

    Remove retired public views and consolidate two-factor setup (#603)
    Co-authored-by: KurzonDax <5052775+KurzonDax@users.noreply.github.com>

--- resources/views/partials/cart-script.blade.php
+++
@@ -1,4 +0,0 @@
-{{-- Cart functionality JavaScript --}}
-{{-- All cart functionality has been moved to resources/js/csp-safe.js for CSP compliance --}}
-{{-- See initCartFunctionality() function --}}
-
